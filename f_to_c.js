7bc220219f236a21f5ede1407641f517af336b64
GuadalupeArgumedoSaucedo <lupeargumedo96@gmail.com>
2024-05-02 15:00:48 -0700

    f to c

---
+++ f_to_c.js
@@ -0,0 +1,8 @@
+function convertFtoC(fahrenheit) {
+    return (fahrenheit - 32) * (5/9);
+  }
+  
+  let currentTemp = 212; //change temp
+  let celsiusTemp = convertFtoC(currentTemp);
+  console.log(`${currentTemp}°F is equal to ${celsiusTemp.toFixed(2)}°C`);
+ 
